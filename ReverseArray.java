10ebe4c9d7db6c71bae2bd5636d46dd51ec5a929
HoangLong205 <hoanglong2kk5@gamil.com>
2025-05-14 20:45:38 +0700

    reverse array 2

--- ReverseArray.java
+++ ReverseArray.java
@@ -1,7 +1,23 @@
+import java.util.Scanner;
+
 public class ReverseArray {
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 4, 5};
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter size of array: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        System.out.println("Enter elements of array: ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
         reverseArray(arr);
+        System.out.print("\n");
+        int[] arr2 = reverseArray2(arr);
+        for (int i = 0; i < arr2.length; i++) {
+            System.out.printf(arr2[i] + " ");
+        }
     }
 
     public static void reverseArray(int[] arr) {
@@ -14,5 +30,14 @@ public class ReverseArray {
         for (int i = 0; i < reversedArr.length; i++) {
             System.out.print(reversedArr[i] + " ");
         }
+    }
+
+    public static int[] reverseArray2(int[] arr) {
+        for (int i = 0; i < arr.length / 2; i++) {
+            int temp = arr[i];
+            arr[i] = arr[arr.length - 1 - i];
+            arr[arr.length - 1 - i] = temp;
+        }
+        return arr;
     }
 }
